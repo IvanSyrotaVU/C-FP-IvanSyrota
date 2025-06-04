28d86aaa26b51e1062f0c0a6d0a37eba4163e4f9
Ivan Syrota <lightsirota1@gmail.com>
2025-06-04 17:05:12 +0300

    Task 2 - Updated Scanner with new file processing

--- C# Ivan/obj/Debug/net8.0-windows/Master.AssemblyInfo.cs
+++ C# Ivan/obj/Debug/net8.0-windows/Master.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Master")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26fc73a9eccdf94d0467e930c787b75e7a935050")]
 [assembly: System.Reflection.AssemblyProductAttribute("Master")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Master")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Scanner/Program.cs
+++ Scanner/Program.cs
@@ -4,6 +4,7 @@ using System.IO.Pipes;
 using System.Linq;
 using System.Text;
 using System.Threading;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 
 class Program
@@ -12,44 +13,69 @@ class Program
     {
         Console.WriteLine("Scanner started.");
 
-        string directory = @"C:\TestScanner"; // <--- Replace with your path
+        string directory = @"C:\TestScanner"; 
 
-        Thread agentA = new Thread(() => RunAgent("pipeA", directory));
-        Thread agentB = new Thread(() => RunAgent("pipeB", directory));
+        var allFiles = Directory.GetFiles(directory, "*.txt");
+        var queueA = new ConcurrentQueue<string>();
+        var queueB = new ConcurrentQueue<string>();
 
-        agentA.Start();
-        agentB.Start();
+        var threads = new List<Thread>();
 
-        agentA.Join();
-        agentB.Join();
+        var filesA = allFiles.Take(allFiles.Length / 2).ToArray();
+        var filesB = allFiles.Skip(allFiles.Length / 2).ToArray();
+
+        foreach (var file in filesA)
+        {
+            var t = new Thread(() => ProcessFile(file, queueA));
+            threads.Add(t);
+            t.Start();
+        }
+
+        foreach (var file in filesB)
+        {
+            var t = new Thread(() => ProcessFile(file, queueB));
+            threads.Add(t);
+            t.Start();
+        }
+
+        
+        foreach (var t in threads)
+            t.Join();
+
+        SendToPipe("pipeA", queueA);
+        SendToPipe("pipeB", queueB);
 
         Console.WriteLine("Transfer completed.");
     }
 
-    static void RunAgent(string pipeName, string dir)
+    static void ProcessFile(string filePath, ConcurrentQueue<string> queue)
     {
-        var results = new List<string>();
+        var text = File.ReadAllText(filePath);
+        var words = text.Split(new[] { ' ', '.', ',', '!', '?', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+        var grouped = words.GroupBy(w => w.ToLower());
 
-        foreach (var file in Directory.GetFiles(dir, "*.txt"))
+        foreach (var g in grouped)
         {
-            var text = File.ReadAllText(file);
-            var words = text.Split(new[] { ' ', '.', ',', '!', '?', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
-            var grouped = words.GroupBy(w => w.ToLower());
-
-            foreach (var g in grouped)
-            {
-                results.Add($"{Path.GetFileName(file)};{g.Key};{g.Count()}");
-            }
+            queue.Enqueue($"{Path.GetFileName(filePath)};{g.Key};{g.Count()}");
         }
 
+        Console.WriteLine($"Processed: {Path.GetFileName(filePath)}");
+    }
+
+    static void SendToPipe(string pipeName, ConcurrentQueue<string> queue)
+    {
         using var pipe = new NamedPipeClientStream(".", pipeName, PipeDirection.Out);
         pipe.Connect();
 
         using var writer = new StreamWriter(pipe, Encoding.UTF8) { AutoFlush = true };
-        foreach (var line in results)
+
+        while (queue.TryDequeue(out var line))
         {
             writer.WriteLine(line);
         }
+
+        Console.WriteLine($"Sent to pipe: {pipeName}");
     }
 }
+
 
